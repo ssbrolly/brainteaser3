8614d4b7df97b394bfe1a52f8fdb403bf23a122b
Usong Cho <silvermouse4012@gmail.com>
2018-03-04 11:29:52 -0700

    initail commit

---
+++ magicNumber.rb
@@ -0,0 +1,20 @@
+
+
+
+
+def magic_number
+  puts "Input here"
+  string = []
+  user_input = gets.to_s
+  
+  string << user_input
+  string.split("").each do |i|
+  
+  
+end
+def maric(choice)
+
+
+
+
+magic_number
